aa4c41d25d3f2470e41115d2371ddea2fac07017
yyr19921203 <123775172@qq.com>
2021-09-16 19:24:45 +1000

    initial the dialog component
    initial the dialog component
    add dialog, List, Item to the component

--- src/client/src/component/CheeseCard/component/DetailDialog.tsx
+++ src/client/src/component/CheeseCard/component/DetailDialog.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 
-const DetailDialog: React.FC = ()=> {
+//UI
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+//Interface
+import { ICartItem } from '../../../interfaces/cart';
+
+interface Props {
+	open: boolean,
+	items: Array<ICartItem>,
+	handleClose: ()=>void
+}
+const DetailDialog: React.FC<Props> = (props)=> {
+	const {
+		open,
+		items,
+		handleClose,
+	} = props
 	return(
-		<div>
-			
-		</div>
+		<Dialog onClose={handleClose} aria-labelledby="cheesse-dialog-title" open={open}>
+			 <DialogTitle id="cheese-dialog-title">Cheese Details</DialogTitle>
+			 <List>
+			 	{items.map((cheese: ICartItem) => (
+					<ListItem button key={cheese.id}>
+						 <ListItemText primary={cheese.title} />
+					</ListItem>
+				))}
+			 </List>
+		</Dialog>
 	)
 }
-
 export default DetailDialog;
